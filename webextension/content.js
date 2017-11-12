d9705ef3e56100acfbf12f8af9b0400671ea8eb6
Calixte Denizet <calixte.denizet@gmail.com>
2017-11-12 17:59:07 +0100

    Fix bug in webext when retrieving patch urls

--- webextension/content.js
+++ webextension/content.js
@@ -20,27 +20,39 @@ if (container) {
         }
         signatures.push("s=" + s);
     });
-    if (signatures) {
+    if (signatures.length != 0) {
         const hgurlPattern = new RegExp("^http[s]?://hg\.mozilla\.org/(?:releases/)?mozilla-([^/]*)/rev/([0-9a-f]+)$");
         const repos = new Set(["central", "beta", "release"]);
-        const hgurls = [];
+        const hgrevs = [];
         let isFirst = false;
+        let currentCommentId = "";
         document.querySelectorAll(".comment-text > a").forEach(a => {
+            const parentId = a.parentNode.attributes.id;
+            if (parentId !== currentCommentId) {
+                // we're in a new comment
+                currentCommentId = parentId;
+                isFirst = false;
+            }
             const prev = a.previousSibling;
             if (prev == null || (prev.previousSibling == null && !prev.textContent.trim())) {
+                // the first element in the comment is the likn (no text before)
                 isFirst = true;
             }
             if (isFirst) {
+                // so we take the first link and the following ones only if they match the pattern
                 const link = a.href;
                 const m = link.match(hgurlPattern);
                 if (m != null) {
                     let repo = m[1];
                     if (repos.has(repo)) {
-                        if (repo == "central") {
+                        if (repo === "central") {
                             repo = "nightly";
                         }
-                        const rev = m[2];
-                        hgurls.push("h=" + repo + "|" + rev);
+                        let rev = m[2];
+                        if (rev.length > 12) {
+                            rev = rev.slice(0, 12);
+                        }
+                        hgrevs.push("h=" + repo + "|" + rev);
                     }
                 }
             }
@@ -48,7 +60,9 @@ if (container) {
         //const crashStop = "https://localhost:5000";
         const crashStop = "https://crash-stop.herokuapp.com";
         const sumup = crashStop + "/sumup.html";
-        const crashStopLink = encodeURI(sumup + "?" + hgurls.join("&") + "&" + signatures.join("&"));
+        const hpart = hgrevs.length != 0 ? (hgrevs.join("&") + "&") : "";
+        const spart = signatures.join("&");
+        const crashStopLink = encodeURI(sumup + "?" + hpart + spart);
         const iframe = document.createElement("iframe");
         const div = document.createElement("div");
         iframe.setAttribute("src", crashStopLink);
